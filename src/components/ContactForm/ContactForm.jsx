1eb670bc7234685714f51af12cbb808e2d592699
gulen13 <vitaliy.gulen@gmail.com>
2023-05-26 15:02:34 +0300

    realised form with react hook form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,39 +1,26 @@
 import PropTypes from 'prop-types';
-// import { useState } from 'react';
 import { Form, Label, Button, Input } from './ContactForm.styled';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const validationSchema = yup
+  .object({
+    name: yup.string().min(3).required(),
+    number: yup.number().positive().integer().required(),
+  })
+  .required();
 
 const ContactForm = ({ formSubmit }) => {
-  // const [name, setname] = useState('');
-  // const [number, setnumber] = useState('');
-
-  // const handleContactSave = e => {
-  //   e.preventDefault();
-  //   formSubmit(name, number);
-  //   setname('');
-  //   setnumber('');
-  // };
-
-  // const handleChange = event => {
-  //   const { name, value } = event.target;
-  //   switch (name) {
-  //     case 'name':
-  //       setname(value);
-  //       break;
-  //     case 'number':
-  //       setnumber(value);
-  //       break;
-  //     default:
-  //       return;
-  //   }
-  // };
-
   const {
     register,
     handleSubmit,
     resetField,
     formState: { errors, isDirty },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
 
   const onSubmit = ({ name, number }) => {
     formSubmit(name, number);
@@ -41,39 +28,35 @@ const ContactForm = ({ formSubmit }) => {
     resetField('number');
   };
 
-  // console.log(watch('example'));
-
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <Label>
         Name
-        <Input
-          // type="text"
-          // name="name"
-          // value={name}
-          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          // required
-          // onChange={handleChange}
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          placeholder="Jacob Mercer"
-          {...register('name', { required: true })}
-        />
-        {errors.name && <span>This field is required</span>}
+        <Input placeholder="Jacob Mercer" {...register('name')} />
+        <p>
+          {errors.name?.message &&
+            Notify.failure(
+              'Name may contain only letters, apostrophe, dash and spaces.',
+              {
+                timeout: 4000,
+                width: '400px',
+              }
+            )}
+        </p>
       </Label>
       <Label>
         Number
-        <Input
-          // type="tel"
-          // name="number"
-          // value={number}
-          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          // required
-          // onChange={handleChange}
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          placeholder="+380675006070"
-          {...register('number', { required: true })}
-        />
-        {errors.number && <span>This field is required</span>}
+        <Input placeholder="+380675006070" {...register('number')} />
+        <p>
+          {errors.number?.message &&
+            Notify.failure(
+              'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
+              {
+                timeout: 4000,
+                width: '400px',
+              }
+            )}
+        </p>
       </Label>
       <Button type="submit" disabled={!isDirty}>
         Add Contact
